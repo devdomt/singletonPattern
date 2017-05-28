07a73175b8dde177f9ae3cd1b44ab54d5062f97c
devdomt <devdomtest@gmail.com>
2017-05-28 09:26:31 +0200

    fixed singleton example class

--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
-
 /**
  * Created by dominik on 28.05.2017.
  */
 public class Singleton {
+
+    private static Singleton singletonInstance;
+
+    private Singleton() {}
+
+    public static Singleton getSingletonInstance() {
+        if (singletonInstance == null) {
+            singletonInstance = new Singleton();
+        }
+        return singletonInstance;
+    }
+
 }
